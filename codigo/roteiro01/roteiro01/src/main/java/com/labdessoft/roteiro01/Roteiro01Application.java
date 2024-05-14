0cb774f0f4106d54b20b2cbff3ebdce9f1723ffa
dudu-dev <55645344+dudu-dev@users.noreply.github.com>
2024-05-14 20:15:30 -0300

    atualiza o projeto

--- codigo/roteiro01/roteiro01/src/main/java/com/labdessoft/roteiro01/Roteiro01Application.java
+++ codigo/roteiro01/roteiro01/src/main/java/com/labdessoft/roteiro01/Roteiro01Application.java
@@ -3,7 +3,6 @@ package com.labdessoft.roteiro01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Roteiro01Application {
 
