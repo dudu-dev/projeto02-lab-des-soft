ef2f6a1d103e70f3983f39dc8f984aba782417cd
dudu-dev <55645344+dudu-dev@users.noreply.github.com>
2024-06-18 20:58:52 -0300

    cria o front sem integração para uma tarefa livre

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TodoWrapper } from './components/TodoWrapper';
+export function App(props) {
+    return (
+        <div className='App'>
+        <TodoWrapper />
+        </div>
+    )
+}
+
